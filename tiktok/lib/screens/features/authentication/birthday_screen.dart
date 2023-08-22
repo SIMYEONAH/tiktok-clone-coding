d1e44d624e258b6558f07be8ebab294aa4d5c376
SIMYEONAH <simya0528@naver.com>
2023-08-23 01:50:24 +0900

    done password page
    비밀번호 입력 창을 다 완성했습니다.
    이모지의 기능도 추가함

---
+++ tiktok/lib/screens/features/authentication/birthday_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BirthDayScreen extends StatelessWidget {
+  const BirthDayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
