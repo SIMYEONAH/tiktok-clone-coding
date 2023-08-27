d89173e8f7c6e4372dfdb2a60ec9f856f7e3e3c4
SIMYEONAH <simya0528@naver.com>
2023-08-27 18:18:27 +0900

    MainNavigationScreen

--- tiktok/lib/screens/features/onboarding/tutorial_screen.dart
+++ tiktok/lib/screens/features/onboarding/tutorial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok/screens/features/main_navigation/main_navigation_screen.dart';
 
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
@@ -41,6 +42,14 @@ class _TutorialScreenState extends State<TutorialScreen> {
         _showingPage = Page.first;
       });
     }
+  }
+
+  void _onEnterAppTap() {
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) => const MainNavigationScreen(),
+        ),
+        (route) => false);
   }
 
   @override
@@ -108,7 +117,7 @@ class _TutorialScreenState extends State<TutorialScreen> {
                 duration: const Duration(milliseconds: 300),
                 opacity: _showingPage == Page.first ? 0 : 1,
                 child: CupertinoButton(
-                  onPressed: () {},
+                  onPressed: _onEnterAppTap,
                   color: Theme.of(context).primaryColor,
                   child: const Text('Enter the app!'),
                 ),
