e26ce56e1d0a1a96c505ea0f51de368f1cc71ba3
SIMYEONAH <simya0528@naver.com>
2023-08-31 01:53:38 +0900

    VideoTimelineScreen

--- tiktok/lib/screens/features/main_navigation/main_navigation_screen.dart
+++ tiktok/lib/screens/features/main_navigation/main_navigation_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tiktok/screens/features/main_navigation/stf_screen.dart';
 import 'package:tiktok/screens/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok/screens/features/main_navigation/widgets/post_video_button.dart';
 
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
+import '../videos/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -48,19 +48,19 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       body: Stack(children: [
         Offstage(
           offstage: _selectedIndex != 0,
-          child: const StfScreen(),
+          child: const VideoTimelineScreen(),
         ),
         Offstage(
           offstage: _selectedIndex != 1,
-          child: const StfScreen(),
+          child: Container(),
         ),
         Offstage(
           offstage: _selectedIndex != 3,
-          child: const StfScreen(),
+          child: Container(),
         ),
         Offstage(
           offstage: _selectedIndex != 4,
-          child: const StfScreen(),
+          child: Container(),
         ),
       ]),
       bottomNavigationBar: BottomAppBar(
