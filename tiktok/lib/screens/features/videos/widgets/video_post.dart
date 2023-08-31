ff9eef1aea04c1f15c2b05594214e9526724c0bb
SIMYEONAH <simya0528@naver.com>
2023-09-01 00:58:16 +0900

    AnimatedBuilder

--- tiktok/lib/screens/features/videos/widgets/video_post.dart
+++ tiktok/lib/screens/features/videos/widgets/video_post.dart
@@ -57,9 +57,6 @@ class _VideoPostState extends State<VideoPost>
       value: 1.5,
       duration: _animationDuration,
     );
-    _animationController.addListener(() {
-      setState(() {});
-    });
   }
 
   @override
@@ -109,8 +106,14 @@ class _VideoPostState extends State<VideoPost>
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: Transform.scale(
-                  scale: _animationController.value,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
                   child: AnimatedOpacity(
                     opacity: _isPaused ? 1 : 0,
                     duration: _animationDuration,
@@ -123,7 +126,7 @@ class _VideoPostState extends State<VideoPost>
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
