fbe639eb2ee9a14c5b1149f6382d3c805e8a5ca4
SIMYEONAH <simya0528@naver.com>
2023-09-05 00:49:12 +0900

    채팅 디테일 챌린지

--- tiktok/lib/screens/features/inbox/chat_detail_screen.dart
+++ tiktok/lib/screens/features/inbox/chat_detail_screen.dart
@@ -4,36 +4,60 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
 
-class ChatDetailScreen extends StatefulWidget {
+class ChatDetailScreen extends StatelessWidget {
   const ChatDetailScreen({super.key});
+  final bool isIphone = false;
 
-  @override
-  State<ChatDetailScreen> createState() => _ChatDetailScreenState();
-}
-
-class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const ListTile(
+        automaticallyImplyLeading: isIphone ? true : false,
+        title: ListTile(
           contentPadding: EdgeInsets.zero,
           horizontalTitleGap: Sizes.size8,
-          leading: CircleAvatar(
-            radius: Sizes.size24,
-            foregroundImage: NetworkImage(
-              "https://cdn.newspenguin.com/news/photo/202010/3276_9692_1455.jpg",
-            ),
-            child: Text('뚱냥'),
+          leading: Stack(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(Sizes.size4),
+                child: CircleAvatar(
+                  foregroundImage: NetworkImage(
+                      "https://cdn.newspenguin.com/news/photo/202010/3276_9692_1455.jpg"),
+                  radius: Sizes.size20,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: Sizes.size18,
+                  height: Sizes.size18,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: Sizes.size3,
+                    ),
+                    borderRadius: BorderRadius.circular(Sizes.size24),
+                  ),
+                ),
+              ),
+            ],
           ),
-          title: Text(
-            '뚱냥',
+          title: const Text(
+            "뚱냥",
             style: TextStyle(
-              fontWeight: FontWeight.w600,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          subtitle: Text('Active now'),
-          trailing: Row(
+          subtitle: const Text(
+            "Active Now",
+            style:
+                TextStyle(fontSize: Sizes.size12, fontWeight: FontWeight.w400),
+          ),
+        ),
+        actions: const [
+          Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               FaIcon(
@@ -47,74 +71,123 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
                 color: Colors.black,
                 size: Sizes.size20,
               ),
+              Gaps.h24,
             ],
           ),
-        ),
+        ],
       ),
-      body: Stack(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
         children: [
-          ListView.separated(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
-              horizontal: Sizes.size14,
-            ),
-            itemBuilder: (context, index) {
-              final isMine = index % 2 == 0;
-              return Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment:
-                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(Sizes.size14),
-                    decoration: BoxDecoration(
-                      color:
-                          isMine ? Colors.blue : Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(
-                          Sizes.size20,
+          Expanded(
+            child: ListView.separated(
+                reverse: true,
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size20,
+                  horizontal: Sizes.size14,
+                ),
+                itemBuilder: (context, index) {
+                  final isMine = index % 2 == 0;
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: isMine
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(Sizes.size14),
+                        decoration: BoxDecoration(
+                          color: !isMine
+                              ? Theme.of(context).primaryColor
+                              : Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topLeft: const Radius.circular(
+                              Sizes.size12,
+                            ),
+                            topRight: const Radius.circular(
+                              Sizes.size12,
+                            ),
+                            bottomLeft: isMine
+                                ? const Radius.circular(
+                                    Sizes.size12,
+                                  )
+                                : Radius.zero,
+                            bottomRight: isMine
+                                ? Radius.zero
+                                : const Radius.circular(
+                                    Sizes.size12,
+                                  ),
+                          ),
                         ),
-                        topRight: const Radius.circular(
-                          Sizes.size20,
-                        ),
-                        bottomLeft: Radius.circular(
-                          isMine ? Sizes.size20 : Sizes.size5,
-                        ),
-                        bottomRight: Radius.circular(
-                          !isMine ? Sizes.size20 : Sizes.size5,
+                        child: const Text(
+                          "Message",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizes.size16,
+                          ),
                         ),
                       ),
-                    ),
-                    child: const Text(
-                      "this is a message!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Sizes.size16,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            },
-            separatorBuilder: (context, index) => Gaps.v10,
-            itemCount: 10,
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) => Gaps.v10,
+                itemCount: 12),
           ),
-          Positioned(
-            bottom: 0,
-            width: MediaQuery.of(context).size.width,
-            child: BottomAppBar(
-              color: Colors.grey.shade50,
-              child: Row(
-                children: [
-                  const Expanded(child: TextField()),
-                  Gaps.h20,
-                  Container(
-                    child: const FaIcon(FontAwesomeIcons.paperPlane),
-                  )
-                ],
-              ),
+          BottomAppBar(
+            color: Colors.grey.shade50,
+            padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size8, horizontal: Sizes.size16),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: TextField(
+                    textInputAction: TextInputAction.send,
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: Sizes.size16,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Send a message...",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size16,
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                      suffixIcon: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.faceLaugh,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                    cursorColor: Theme.of(context).primaryColor,
+                  ),
+                ),
+                Gaps.h16,
+                Container(
+                  padding: const EdgeInsets.all(Sizes.size8),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey.shade400,
+                  ),
+                  child: const FaIcon(
+                    FontAwesomeIcons.solidPaperPlane,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
