15ef749de0c52898ab0cb535eb573fc1cf9214aa
SIMYEONAH <simya0528@naver.com>
2023-09-05 23:49:55 +0900

    SwitchListTile

--- tiktok/lib/screens/features/settings/settings_screen.dart
+++ tiktok/lib/screens/features/settings/settings_screen.dart
@@ -1,35 +1,77 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: ListWheelScrollView(
-          diameterRatio: 1.5,
-          offAxisFraction: 1.5,
-          itemExtent: 200,
-          children: [
-            for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: Container(
-                  color: Colors.teal,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Pick me',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 39,
-                    ),
-                  ),
-                ),
-              )
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          SwitchListTile.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("Enable notifications"),
+          ),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+          ),
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black)),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          const AboutListTile(),
+        ],
+      ),
+    );
   }
 }
