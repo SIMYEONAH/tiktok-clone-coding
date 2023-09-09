5ecf4c6657550841c260b4b54a01d4bc2f469764
SIMYEONAH <simya0528@naver.com>
2023-09-09 20:44:53 +0900

    TextTheme

--- tiktok/lib/screens/features/authentication/sign_up_screen.dart
+++ tiktok/lib/screens/features/authentication/sign_up_screen.dart
@@ -7,6 +7,8 @@ import 'package:tiktok/screens/features/authentication/login_screen.dart';
 import 'package:tiktok/screens/features/authentication/username_screen.dart';
 import 'package:tiktok/screens/features/authentication/widgets/auth_button.dart';
 
+import '../../../utils.dart';
+
 class SignUpScreen extends StatelessWidget {
   static const routeURL = "/";
   static const routeName = "signUp";
@@ -57,21 +59,21 @@ class SignUpScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Gaps.v80,
-                  const Text(
+                  Text(
                     "Sign up for TikTok",
-                    style: TextStyle(
-                      fontSize: Sizes.size24,
-                      fontWeight: FontWeight.w700,
-                    ),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(color: Colors.red),
                   ),
                   Gaps.v20,
-                  const Text(
-                    "Create a profile, follow other accounts, make your own videos, and more.",
-                    style: TextStyle(
-                      fontSize: Sizes.size16,
-                      color: Colors.black45,
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      "Create a profile, follow other accounts, make your own videos, and more.",
+                      style: Theme.of(context).textTheme.titleMedium,
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
                   ),
                   Gaps.v40,
                   if (orientation == Orientation.portrait) ...[
@@ -114,7 +116,7 @@ class SignUpScreen extends StatelessWidget {
             ),
           ),
           bottomNavigationBar: BottomAppBar(
-            color: Colors.grey.shade50,
+            color: isDarkMode(context) ? null : Colors.grey.shade50,
             elevation: 2,
             child: Padding(
               padding: const EdgeInsets.symmetric(
