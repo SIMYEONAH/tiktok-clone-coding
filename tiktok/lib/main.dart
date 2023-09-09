a372e06e0af53cec719d8fc186de6642381c4591
SIMYEONAH <simya0528@naver.com>
2023-09-09 21:12:25 +0900

    google fonts

--- tiktok/lib/main.dart
+++ tiktok/lib/main.dart
@@ -32,35 +32,8 @@ class TikTokApp extends StatelessWidget {
       title: 'TikTok Clone',
       themeMode: ThemeMode.system,
       theme: ThemeData(
+        textTheme: GoogleFonts.itimTextTheme(),
         brightness: Brightness.light,
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-              fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          displayMedium: GoogleFonts.openSans(
-              fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          displaySmall:
-              GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-          headlineMedium: GoogleFonts.openSans(
-              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headlineSmall:
-              GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-          titleLarge: GoogleFonts.openSans(
-              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          titleMedium: GoogleFonts.openSans(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          titleSmall: GoogleFonts.openSans(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyLarge: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyMedium: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          labelLarge: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          bodySmall: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          labelSmall: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
         textSelectionTheme: const TextSelectionThemeData(
@@ -79,6 +52,9 @@ class TikTokApp extends StatelessWidget {
         ),
       ),
       darkTheme: ThemeData(
+        textTheme: GoogleFonts.itimTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
         bottomAppBarTheme: BottomAppBarTheme(
