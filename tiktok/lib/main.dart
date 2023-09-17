d2575b0611f317704648748a7cfdcf7458f773b3
SIMYEONAH <simya0528@naver.com>
2023-09-18 01:59:21 +0900

    파이어베이스 설치 완료

--- tiktok/lib/main.dart
+++ tiktok/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,9 +8,14 @@ import 'package:tiktok/screens/features/videos/repos/playback_config_repos.dart'
 import 'package:tiktok/screens/features/videos/view_models/playback_config_vm.dart';
 
 import 'constants/sizes.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations(
     [
